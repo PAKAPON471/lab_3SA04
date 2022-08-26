eb453a3db3bf4a8dea2f3210432ddb3b2d3eec67
unknown <6310110345@psu.ac.th>
2022-08-27 04:00:06 +0700

    fourth commit

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Weather from './components/Weather';
 import Constants from 'expo-constants';
 
 export default function App() {
- const doIt = () => {
-  console.log("Hello from console")
- }
  return (
   <View style={styles.container}>
     <Weather zipCode="90110"/>
-    <StatusBar style="auto" />
+    <StatusBar style="auto"/>
   </View>
  );
 }
