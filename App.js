d5ae098fb66445f3872c3c2ed5e8bd7875f22d68
unknown <6310110345@psu.ac.th>
2022-08-27 02:35:49 +0700

    second commit

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather';
+import Constants from 'expo-constants';
 
 export default function App() {
  const doIt = () => {
@@ -8,7 +10,7 @@ export default function App() {
  }
  return (
   <View style={styles.container}>
-    <Text onPress={doIt}>Hello world</Text>
+    <Weather zipCode="90110"/>
     <StatusBar style="auto" />
   </View>
  );
@@ -16,9 +18,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
  container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
+  paddingTop: Constants.statusBarHeight
  },
 });
